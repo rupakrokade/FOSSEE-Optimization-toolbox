a1f5ad09fb75299578eee3447e194965ce475505
Siddharth11235 <siddharth11235@gmail.com>
2019-03-28 12:46:49 +0530

    cleanup
    Former-commit-id: 09740c62df8debf19e6e76560f88bb650ee9dcf4

--- sci_gateway/cpp/libskeleton_cpp.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LIBSKELETON_CPP_GW_H__
-#define __LIBSKELETON_CPP_GW_H__
-
-#include "c_gateway_prototype.h"
-
-
-STACK_GATEWAY_PROTOTYPE(sci_cpp_find);
-
-#endif /* __LIBSKELETON_CPP_GW_H__ */
